be0ecc80f033b84046332e4cb99a354c67872cfa
tnty <tnty.working@gmail.com>
2022-09-14 18:26:31 +0300

    feat: AppRoutes

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { useRoutes } from 'react-router-dom';
+
+import MainPage from '../pages/MainPage/MainPage';
+import CollectionsPage from "../pages/CollectionsPage/CollectionsPage";
+import PicturesPage from "../pages/PicturesPage/PicturesPage";
+
+const AppRoutes = () => useRoutes([
+    {path: "/", element: <MainPage/>},
+    {path: "/collections", element: <CollectionsPage/>},
+    {path: "/pictures", element: <PicturesPage/>},
+]);
+
+export default AppRoutes;
