9c033ec83d69afb2b21e7b02a80fa33fdb2650f4
tnty <tnty.working@gmail.com>
2022-09-15 10:41:23 +0300

    feat: Search component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from 'react';
 import styles from './Header.module.scss';
 import Container from "components/Container/Container";
+import Logo from "components/Logo/Logo";
+import Search from "components/Search/Search";
 
 const Header: FC = () => {
   return (
       <header className={styles.header}>
         <Container>
             <div className={styles.header__wrapper}>
-                <h1></h1>
+                <Logo />
+                <Search />
             </div>
         </Container>
       </header>
