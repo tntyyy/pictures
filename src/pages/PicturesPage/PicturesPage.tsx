ed1d5cd4cafc9a1cd1091aff942c82bde1182b84
tnty <tnty.working@gmail.com>
2022-09-15 16:49:46 +0300

    feat: pictures page

--- src/pages/PicturesPage/PicturesPage.tsx
+++ src/pages/PicturesPage/PicturesPage.tsx
@@ -1,9 +1,28 @@
 import React, { FC } from 'react';
 import styles from './PicturesPage.module.scss';
+import {IPicture} from "../../types/pictures";
+import Container from "../../components/Container/Container";
+import Title from "../../components/Titile/Title";
+import PicturesList from "../../components/PicturesList/PicturesList";
+
+const mockPictures: IPicture[] = [
+  {id: 1, title: 'Lorem', path: 'https://stockmeier-food.ru/uploads/posts/2021-11/medium/1637312162_aromatizator-vanil.jpg', collection_id: 1},
+  {id: 2, title: 'Lorem', path: 'https://stockmeier-food.ru/uploads/posts/2021-11/medium/1637312162_aromatizator-vanil.jpg', collection_id: 1},
+  {id: 3, title: 'Lorem', path: 'https://stockmeier-food.ru/uploads/posts/2021-11/medium/1637312162_aromatizator-vanil.jpg', collection_id: 1}
+];
 
 const PicturesPage: FC = () => {
 
-  return (<h1>Pictures Page</h1>);
+  return (
+      <main className={styles.main}>
+        <Container>
+          <div className={styles.wrapper}>
+            <Title>Все картинки</Title>
+            <PicturesList items={mockPictures}/>
+          </div>
+        </Container>
+      </main>
+  );
 };
 
 export default PicturesPage;
