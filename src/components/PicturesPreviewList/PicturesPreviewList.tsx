66947f17747b8fcef364f1ed14ecf3932db0f2ad
tnty <tnty.working@gmail.com>
2022-09-15 12:33:50 +0300

    feat: pictures preview page

---
+++ src/components/PicturesPreviewList/PicturesPreviewList.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from 'react';
+import styles from './PicturesPreviewList.module.scss';
+import {IPicture} from "types/pictures";
+import Title from "../Titile/Title";
+import {Link} from "react-router-dom";
+import ButtonLink from "../ButtonLink/ButtonLink";
+
+interface PicturesPreviewListProps {
+    items: IPicture[];
+}
+
+const PicturesPreviewList: FC<PicturesPreviewListProps> = ({items}) => {
+  return (
+      <div className={styles.list}>
+          <Title>Последние картинки</Title>
+          <div className={styles.wrapper}>
+              {items.map(item =>
+                  <Link
+                      to="/pictures"
+                      key={item.id}
+                      className={styles.item}
+                  >
+                      <img src={item.path} alt={item.title}/>
+                      <p>{item.title}</p>
+                  </Link>
+              )}
+          </div>
+          <div className={styles.btnWrapper}>
+              <ButtonLink path="/pictures" type="filled">Все</ButtonLink>
+          </div>
+      </div>
+  );
+};
+
+export default PicturesPreviewList;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,19 +2,28 @@ import React, { FC } from 'react';
 import styles from './MainPage.module.scss';
 import Container from "components/Container/Container";
 import CollectionsPreviewList from "components/CollectionsPreviewList/CollectionsPreviewList";
-import {ICollection} from "../../types/collections";
+import {ICollection} from "types/collections";
+import {IPicture} from "types/pictures";
+import PicturesPreviewList from "components/PicturesPreviewList/PicturesPreviewList";
 
 const mockCollections: ICollection[] = [
     {id: 1, title: 'Anime', description: 'fsdgsd'},
     {id: 2, title: 'Natural', description: 'fsdgsd'},
     {id: 3, title: 'Animals', description: 'fsdgsd'}
-]
+];
+
+const mockPictures: IPicture[] = [
+    {id: 1, title: 'Lorem', path: 'https://stockmeier-food.ru/uploads/posts/2021-11/medium/1637312162_aromatizator-vanil.jpg', collection_id: 1},
+    {id: 2, title: 'Lorem', path: 'https://stockmeier-food.ru/uploads/posts/2021-11/medium/1637312162_aromatizator-vanil.jpg', collection_id: 1},
+    {id: 3, title: 'Lorem', path: 'https://stockmeier-food.ru/uploads/posts/2021-11/medium/1637312162_aromatizator-vanil.jpg', collection_id: 1}
+];
 
 const MainPage: FC = () => {
   return (
       <main className={styles.main}>
         <Container>
             <CollectionsPreviewList items={mockCollections}/>
+            <PicturesPreviewList items={mockPictures}/>
         </Container>
       </main>
   );
