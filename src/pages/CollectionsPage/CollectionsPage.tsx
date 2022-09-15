40e7f42ad12b8db540ebaf6747a3567ca5aeb9de
tnty <tnty.working@gmail.com>
2022-09-15 15:42:44 +0300

    feat: all collections page

--- src/pages/CollectionsPage/CollectionsPage.tsx
+++ src/pages/CollectionsPage/CollectionsPage.tsx
@@ -1,9 +1,31 @@
 import React, { FC } from 'react';
 import styles from './CollectionsPage.module.scss';
+import Container from "components/Container/Container";
+import {ICollection} from "types/collections";
+import CollectionsList from "components/CollectionsList/CollectionsList";
+import Title from "components/Titile/Title";
+
+const mockCollection: ICollection[] = [
+    {id: 1, title: 'Anime', description: 'fdsfs'},
+    {id: 2, title: 'Anime', description: 'fdsfs'},
+    {id: 3, title: 'Anime', description: 'fdsfs'},
+    {id: 4, title: 'Anime', description: 'fdsfs'},
+    {id: 5, title: 'Anime', description: 'fdsfs'},
+    {id: 6, title: 'Anime', description: 'fdsfs'}
+];
 
 const CollectionsPage: FC = () => {
 
-  return (<h1>Collections Page</h1>);
+  return (
+      <main className={styles.main}>
+        <Container>
+          <div className={styles.wrapper}>
+            <Title>Все коллекции</Title>
+            <CollectionsList items={mockCollection}/>
+          </div>
+        </Container>
+      </main>
+  );
 };
 
 export default CollectionsPage;
