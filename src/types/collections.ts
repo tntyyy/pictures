943094c1eb5eacff726991ba4ce6df296e0558aa
tnty <tnty.working@gmail.com>
2022-09-17 18:47:20 +0300

    feat: create store & collection reducer

--- src/types/collections.ts
+++ src/types/collections.ts
@@ -3,3 +3,34 @@ export interface ICollection {
     title: string,
     description: string
 }
+
+export interface CollectionsState {
+    collections: ICollection[];
+    loading: boolean;
+    error: null | string;
+}
+
+export enum CollectionsActionTypes {
+    FETCH_COLLECTIONS = "FETCH_COLLECTIONS",
+    FETCH_COLLECTIONS_SUCCESS = "FETCH_COLLECTIONS_SUCCESS",
+    FETCH_COLLECTIONS_ERROR = "FETCH_COLLECTIONS_ERROR",
+}
+
+interface FetchCollectionsAction {
+    type: CollectionsActionTypes.FETCH_COLLECTIONS;
+}
+
+interface FetchCollectionsSuccessAction {
+    type: CollectionsActionTypes.FETCH_COLLECTIONS_SUCCESS;
+    payload: ICollection[];
+}
+
+interface FetchCollectionErrorAction {
+    type: CollectionsActionTypes.FETCH_COLLECTIONS_ERROR;
+    payload: string;
+}
+
+export type CollectionsActions =
+    | FetchCollectionsAction
+    | FetchCollectionsSuccessAction
+    | FetchCollectionErrorAction;
