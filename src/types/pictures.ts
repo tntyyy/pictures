f34fe17fd0f586fee184f0241099e1760f80765e
tnty <tnty.working@gmail.com>
2022-09-19 22:25:10 +0300

    feat: types for pictures

--- src/types/pictures.ts
+++ src/types/pictures.ts
@@ -4,3 +4,34 @@ export interface IPicture {
     path: string,
     collection_id: number
 }
+
+export interface PicturesState {
+    pictures: IPicture[];
+    loading: boolean;
+    error: null | string;
+}
+
+export enum PicturesActionTypes {
+    FETCH_PICTURES = "FETCH_PICTURES",
+    FETCH_PICTURES_SUCCESS = "FETCH_PICTURES_SUCCESS",
+    FETCH_PICTURES_ERROR = "FETCH_PICTURES_ERROR"
+}
+
+interface FetchPicturesAction {
+    type: PicturesActionTypes.FETCH_PICTURES;
+}
+
+interface FetchPicturesSuccessAction {
+    type: PicturesActionTypes.FETCH_PICTURES_SUCCESS;
+    payload: IPicture[];
+}
+
+interface FetchPicturesErrorAction {
+    type: PicturesActionTypes.FETCH_PICTURES_ERROR;
+    payload: string;
+}
+
+export type PicturesActions =
+    | FetchPicturesAction
+    | FetchPicturesSuccessAction
+    | FetchPicturesErrorAction
