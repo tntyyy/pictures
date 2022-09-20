61c5fcb64464f93f3a60f5a3d5bbeb166a3873af
tnty <tnty.working@gmail.com>
2022-09-20 19:11:33 +0300

    refactor: collections page

--- src/pages/CollectionsPage/CollectionsPage.tsx
+++ src/pages/CollectionsPage/CollectionsPage.tsx
@@ -1,21 +1,11 @@
 import React, {FC, useEffect} from 'react';
 import styles from './CollectionsPage.module.scss';
 import Container from "components/Container/Container";
-import {ICollection} from "types/collections";
 import CollectionsList from "components/CollectionsList/CollectionsList";
 import Title from "components/Titile/Title";
-import {useTypedSelector} from "../../hooks/useTypedSelector";
-import {useActions} from "../../hooks/useActions";
-import Spinner from "../../components/Spinner/Spinner";
-
-const mockCollection: ICollection[] = [
-    {id: 1, title: 'Anime', description: 'fdsfs'},
-    {id: 2, title: 'Anime', description: 'fdsfs'},
-    {id: 3, title: 'Anime', description: 'fdsfs'},
-    {id: 4, title: 'Anime', description: 'fdsfs'},
-    {id: 5, title: 'Anime', description: 'fdsfs'},
-    {id: 6, title: 'Anime', description: 'fdsfs'}
-];
+import {useTypedSelector} from "hooks/useTypedSelector";
+import {useActions} from "hooks/useActions";
+import Spinner from "components/Spinner/Spinner";
 
 const CollectionsPage: FC = () => {
 
