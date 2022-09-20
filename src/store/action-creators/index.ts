4cd213eb752e8c7293868e86139acaf690888527
tnty <tnty.working@gmail.com>
2022-09-20 13:18:25 +0300

    feat: index export action creators

---
+++ src/store/action-creators/index.ts
@@ -0,0 +1,7 @@
+import * as PicturesActionCreators from './pictures';
+import * as CollectionsActionCreators from './collections';
+
+export default {
+    ...PicturesActionCreators,
+    ...CollectionsActionCreators
+}
