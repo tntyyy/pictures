25dcfc446a8777e7d2ff759d03d98e5416c48538
tnty <tnty.working@gmail.com>
2022-09-20 14:05:44 +0300

    feat: create constants for api request

---
+++ src/constants/api.ts
@@ -0,0 +1,6 @@
+export const BASE_URL = "http://localhost:8080/api/";
+
+export enum pictureEndpoints {
+    PICTURES = 'pictures',
+    PICTURES_IN_COLLECTIONS = 'PICTURES_IN_COLLECTIONS'
+}
--- src/store/action-creators/pictures.ts
+++ src/store/action-creators/pictures.ts
@@ -1,12 +1,13 @@
 import {Dispatch} from "redux";
 import axios from "axios";
 import {PicturesActions, PicturesActionTypes} from "types/pictures";
+import {BASE_URL, pictureEndpoints} from "constants/api";
 
 export const getPictures = () => {
     return async (dispatch: Dispatch<PicturesActions>) => {
         try {
             dispatch({type: PicturesActionTypes.FETCH_PICTURES});
-            const response = await axios.get('http://localhost:8080/api/pictures');
+            const response = await axios.get(`${BASE_URL}${pictureEndpoints.PICTURES}`);
             dispatch({
                 type: PicturesActionTypes.FETCH_PICTURES_SUCCESS,
                 payload: response.data
@@ -16,6 +17,17 @@ export const getPictures = () => {
                 type: PicturesActionTypes.FETCH_PICTURES_ERROR,
                 payload: "Произошла ошибка при загрузке картинок"
             });
+        }
+    }
+}
+
+export const getPicturesByCollection = () => {
+    return async (dispatch: Dispatch<PicturesActions>) => {
+        try {
+            dispatch({type: PicturesActionTypes.FETCH_PICTURES});
+            const response = await axios.get('http://localhost:8080/api/pictures-in-collection/1');
+        } catch (e) {
+
         }
     }
 }
