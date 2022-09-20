21a61c2f4ec723ee1455d9b72358ea3ed1b24fcb
tnty <tnty.working@gmail.com>
2022-09-20 13:43:01 +0300

    feat: fetch pictures in picturesPage

--- src/pages/PicturesPage/PicturesPage.tsx
+++ src/pages/PicturesPage/PicturesPage.tsx
@@ -1,24 +1,33 @@
-import React, { FC } from 'react';
+import React, {FC, useEffect} from 'react';
 import styles from './PicturesPage.module.scss';
-import {IPicture} from "../../types/pictures";
-import Container from "../../components/Container/Container";
-import Title from "../../components/Titile/Title";
-import PicturesList from "../../components/PicturesList/PicturesList";
-
-const mockPictures: IPicture[] = [
-  {id: 1, title: 'Lorem', path: 'https://stockmeier-food.ru/uploads/posts/2021-11/medium/1637312162_aromatizator-vanil.jpg', collection_id: 1},
-  {id: 2, title: 'Lorem', path: 'https://stockmeier-food.ru/uploads/posts/2021-11/medium/1637312162_aromatizator-vanil.jpg', collection_id: 1},
-  {id: 3, title: 'Lorem', path: 'https://stockmeier-food.ru/uploads/posts/2021-11/medium/1637312162_aromatizator-vanil.jpg', collection_id: 1}
-];
+import Container from "components/Container/Container";
+import Title from "components/Titile/Title";
+import PicturesList from "components/PicturesList/PicturesList";
+import {useTypedSelector} from "hooks/useTypedSelector";
+import {useActions} from "hooks/useActions";
 
 const PicturesPage: FC = () => {
+    const {pictures, loading, error} = useTypedSelector(state => state.pictures);
+    const {getPictures} = useActions();
+
+    useEffect(() => {
+        getPictures();
+    }, []);
+
+    if (loading) {
+        return <h1>Загрузка</h1> //todo изменить на спиннер
+    }
+
+    if (error) {
+        return <h1>Ошибка</h1> //todo изменить на спиннер
+    }
 
   return (
       <main className={styles.main}>
         <Container>
           <div className={styles.wrapper}>
             <Title>Все картинки</Title>
-            <PicturesList items={mockPictures}/>
+            <PicturesList items={pictures}/>
           </div>
         </Container>
       </main>
