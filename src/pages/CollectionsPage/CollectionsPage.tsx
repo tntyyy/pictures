25b4115cbf1ab1b4c833abba0a0260b1259740d7
tnty <tnty.working@gmail.com>
2022-09-20 19:10:50 +0300

    feat: fetch & show collections in collections page

--- src/pages/CollectionsPage/CollectionsPage.tsx
+++ src/pages/CollectionsPage/CollectionsPage.tsx
@@ -1,9 +1,12 @@
-import React, { FC } from 'react';
+import React, {FC, useEffect} from 'react';
 import styles from './CollectionsPage.module.scss';
 import Container from "components/Container/Container";
 import {ICollection} from "types/collections";
 import CollectionsList from "components/CollectionsList/CollectionsList";
 import Title from "components/Titile/Title";
+import {useTypedSelector} from "../../hooks/useTypedSelector";
+import {useActions} from "../../hooks/useActions";
+import Spinner from "../../components/Spinner/Spinner";
 
 const mockCollection: ICollection[] = [
     {id: 1, title: 'Anime', description: 'fdsfs'},
@@ -16,12 +19,28 @@ const mockCollection: ICollection[] = [
 
 const CollectionsPage: FC = () => {
 
+    const {collections, loading, error} = useTypedSelector(state => state.collections);
+    const {getCollections} = useActions();
+
+    useEffect(() => {
+        getCollections();
+    }, []);
+
+    if (loading) {
+        return (<Spinner/>)
+    }
+
+
+    if (error) {
+        return (<h1>Произошла ошибка</h1>)
+    }
+
   return (
       <main className={styles.main}>
         <Container>
           <div className={styles.wrapper}>
             <Title>Все коллекции</Title>
-            <CollectionsList items={mockCollection}/>
+            <CollectionsList items={collections}/>
           </div>
         </Container>
       </main>
