a416308516df5d41b66f770d17fb1bf5bb651900
tnty <tnty.working@gmail.com>
2022-09-20 13:46:07 +0300

    feat: fetch pictures in mainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,11 @@
-import React, { FC } from 'react';
+import React, {FC, useEffect} from 'react';
 import styles from './MainPage.module.scss';
 import Container from "components/Container/Container";
 import CollectionsPreviewList from "components/CollectionsPreviewList/CollectionsPreviewList";
 import {ICollection} from "types/collections";
-import {IPicture} from "types/pictures";
 import PicturesPreviewList from "components/PicturesPreviewList/PicturesPreviewList";
+import {useTypedSelector} from "hooks/useTypedSelector";
+import {useActions} from "hooks/useActions";
 
 const mockCollections: ICollection[] = [
     {id: 1, title: 'Anime', description: 'fsdgsd'},
@@ -12,19 +13,19 @@ const mockCollections: ICollection[] = [
     {id: 3, title: 'Animals', description: 'fsdgsd'}
 ];
 
-const mockPictures: IPicture[] = [
-    {id: 1, title: 'Lorem', path: 'https://stockmeier-food.ru/uploads/posts/2021-11/medium/1637312162_aromatizator-vanil.jpg', collection_id: 1},
-    {id: 2, title: 'Lorem', path: 'https://stockmeier-food.ru/uploads/posts/2021-11/medium/1637312162_aromatizator-vanil.jpg', collection_id: 1},
-    {id: 3, title: 'Lorem', path: 'https://stockmeier-food.ru/uploads/posts/2021-11/medium/1637312162_aromatizator-vanil.jpg', collection_id: 1}
-];
-
 const MainPage: FC = () => {
+    const {pictures, error, loading} = useTypedSelector(state => state.pictures);
+    const {getPictures} = useActions();
+
+    useEffect(() => {
+        getPictures();
+    }, []);
   return (
       <main className={styles.main}>
         <Container>
             <div className={styles.wrapper}>
                 <CollectionsPreviewList items={mockCollections}/>
-                <PicturesPreviewList items={mockPictures}/>
+                <PicturesPreviewList items={pictures.slice(0, 3)}/>
             </div>
         </Container>
       </main>
